a9a0edc2d4948290a62f1a2bbc809624aef007ec
Ali Sbane <ali.sbane.dev@gmail.com>
2022-01-25 16:40:13 +0100

    App Completed

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
-const App = {
-  components: {
-    QrCodeImage
-  }
-}
-
-const QrCodeImage = {
+const QrCodeImageWrapper = {
   template: `
   <div class="image-wrapper">
-    <img src="" alt="QR code" title="QR code"/>
+    <img src="./images/image-qr-code.png" alt="QR code" title="QR code"/>
   </div>
-  `
-}
+  `,
+};
 
-Vue.createApp(App).mount('#app')
+const H1Heading = {
+  template: `
+  <h1>{{ h1Text }}</h1>
+  `,
+  data() {
+    return {
+      h1Text: "Improve your front-end skills by building projects",
+    };
+  },
+};
+
+const CardPara = {
+  template: `
+  <p>{{ pText }}</p>
+  `,
+  data() {
+    return {
+      pText:
+        "Scan the QR code to visit Frontend Mentor and take your coding skills to the next level",
+    };
+  },
+};
+
+const App = {
+  components: {
+    QrCodeImageWrapper,
+    H1Heading,
+    CardPara,
+  },
+};
+
+Vue.createApp(App).mount("#app");
